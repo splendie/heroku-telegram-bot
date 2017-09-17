05c2122ea2d76ba2e4def4aa203161cfc82800dc
splendie <indri.yunita@ymail.com>
2017-09-17 12:16:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,8 +38,7 @@ def newgame(bot, update):
     update.message.reply_text('We will start a new game of \"Undercover\". You will be given a word that you should not tell to anyone. In each round, everyone will have to describe the word. In this game, at least one of you are an undercover. In the beginning of the game, no one knows who is the undercover, including the undercover themselves. After each round, players can decide who is the undercover. Voice of majority wins. \n\nNow type \"/join\" if you want to join the game. Type \"/startgame\" after everyone has joined.')
 
 def join(bot, update):
-	user = update.message.effective_user()
-update.message.reply_text(user)
+  update.message.reply_text(update.message.effective_user())
 	
 def help(bot, update):
     update.message.reply_text('Help!')
